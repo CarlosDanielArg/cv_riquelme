9dbec0dc57469825b0207a7c3cf40fc53dedb3df
Carlos Daniel <113786320+CarlosDanielArg@users.noreply.github.com>
2024-07-29 22:46:08 -0300

    Add files via upload

--- js/coloca_paneles.js
+++ js/coloca_paneles.js
@@ -1,17 +1,26 @@
-document.getElementById("boton_datos_personales").addEventListener("click", function(){
-    document.getElementById("panel_datos_personales").style.display = "block";
-    document.getElementById("panel_trayectoria").style.display = "none";
-    document.getElementById("panel_titulos").style.display = "none";
-});
+// document.getElementById("boton_datos_personales").addEventListener("click", function(){
+//     document.getElementById("panel_datos_personales").style.display = "block";
+//     document.getElementById("panel_trayectoria").style.display = "none";
+//     document.getElementById("panel_titulos").style.display = "none";
+// });
 
-document.getElementById("boton_trayectoria").addEventListener("click", function(){
-    document.getElementById("panel_datos_personales").style.display = "none";
-    document.getElementById("panel_trayectoria").style.display = "block";
-    document.getElementById("panel_titulos").style.display = "none";
-});
+// document.getElementById("boton_trayectoria").addEventListener("click", function(){
+//     document.getElementById("panel_datos_personales").style.display = "none";
+//     document.getElementById("panel_trayectoria").style.display = "block";
+//     document.getElementById("panel_titulos").style.display = "none";
+// });
 
-document.getElementById("boton_titulos").addEventListener("click", function(){
-    document.getElementById("panel_datos_personales").style.display = "none";
-    document.getElementById("panel_trayectoria").style.display = "none";
-    document.getElementById("panel_titulos").style.display = "block";
-});
+// document.getElementById("boton_titulos").addEventListener("click", function(){
+//     document.getElementById("panel_datos_personales").style.display = "none";
+//     document.getElementById("panel_trayectoria").style.display = "none";
+//     document.getElementById("panel_titulos").style.display = "block";
+// });
+
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            vista: "datos_personales",
+        }
+    }
+}).mount('#app')
